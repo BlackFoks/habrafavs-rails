51ba2fe1f0ef1be877441d3da2cb5fff08de2d39
blackfoks <mail@blackfoks.com>
2011-08-26 14:14:24 +0700

    Init commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+Habrafavs
+=========
+
+Habrafavs provides you a way to store your habrahabr favorites safety in the cloud.
+
+Authors
+-------
+blackfoks (mail@blackfoks.com)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HabrafavsRails::Application.config.secret_token = 'fe6f4a8ed16399fc5153c6cec41db36bc2ab2175af60e6694f433ee29a7adfedc07c6e3a122cb84bbde5fd9a96ac3f32d80b87c692d9dc1c5899057d3b73a1ce'
