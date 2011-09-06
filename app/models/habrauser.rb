44e93bb3eb3f941f3f82707288f5b150204d48c9
blackfoks <mail@blackfoks.com>
2011-09-06 13:37:48 +0700

    Habrausers' name and slug now must be unique.

--- app/models/habrauser.rb
+++ app/models/habrauser.rb
@@ -2,6 +2,7 @@ class Habrauser < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :name, :slug
+  validates_uniqueness_of :name, :slug
 
   def habraurl
     "http://#{self.slug}.habrahabr.ru"
