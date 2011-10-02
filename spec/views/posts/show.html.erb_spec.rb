319aceb3b6fc4fc5ec4c8dfd777e0801a4608c27
blackfoks <mail@blackfoks.com>
2011-10-02 23:45:48 +0700

    Fix posts/_form and posts/show view spec

--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -3,22 +3,21 @@ require 'spec_helper'
 describe "posts/show.html.erb" do
   before(:each) do
     @post = assign(:post, stub_model(Post,
-      :blog => nil,
+      :blog => stub_model(Blog, :name => "Ruby on Rails"),
       :title => "Title",
       :content => "Content",
-      :author => nil
+      :author => stub_model(Habrauser, :name => "Alizar")
     ))
   end
 
   it "renders attributes in <p>" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(//)
+    rendered.should match(/Ruby on Rails/)
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Title/)
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Content/)
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(//)
+    rendered.should match(/Alizar/)
   end
 end
