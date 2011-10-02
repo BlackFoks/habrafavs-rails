7e9072b525dbe5e519846465842fd3ce19fb23f6
blackfoks <mail@blackfoks.com>
2011-10-02 23:43:49 +0700

    Fix favs/index view and spec

--- spec/views/favs/index.html.erb_spec.rb
+++ spec/views/favs/index.html.erb_spec.rb
@@ -2,23 +2,18 @@ require 'spec_helper'
 
 describe "favs/index.html.erb" do
   before(:each) do
+    @habrauser = stub_model(Habrauser, :name => "Alizar")
+    @post = stub_model(Post, :title => "Title")
+
     assign(:favs, [
-      stub_model(Fav,
-        :user => nil,
-        :post => nil
-      ),
-      stub_model(Fav,
-        :user => nil,
-        :post => nil
-      )
+      stub_model(Fav, :habrauser => @habrauser, :post => @post),
+      stub_model(Fav, :habrauser => @habrauser, :post => @post)
     ])
   end
 
   it "renders a list of favs" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => nil.to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => nil.to_s, :count => 2
+    assert_select "tr>td>a", :text => @habrauser.name, :count => 2
+    assert_select "tr>td>a", :text => @post.title, :count => 2
   end
 end
