5f848b204f0a4aaab49aa6a28865cbd29e9a5826
blackfoks <mail@blackfoks.com>
2011-10-02 16:28:10 +0700

    Add Fav#load_from_page

--- app/models/fav.rb
+++ app/models/fav.rb
@@ -1,4 +1,12 @@
 class Fav < ActiveRecord::Base
   belongs_to :habrauser
   belongs_to :post
+
+  # parses favs page
+  def self.load_from_page(habrauser, page)
+    page.each do |fav|
+      post = Post.find_or_fetch(fav.post.id)
+      Fav.create :habrauser => habrauser, :post => post
+    end
+  end
 end
